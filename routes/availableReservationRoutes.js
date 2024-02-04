8a6276a3b9f3346cb83e67176253e234e31cc8e8
pausm99 <pausm99@gmail.com>
2024-02-04 19:10:37 +0100

    ADD: available reservation routes

--- routes/availableReservationRoutes.js
+++ routes/availableReservationRoutes.js
@@ -4,5 +4,11 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.route('/byField/:id').get(authMiddleware.authenticateToken, availablereservationController.getAvailableReservationsByFieldId);
+router.route('/occupied/:id').get(authMiddleware.authenticateToken, availablereservationController.getAllTypeReservationsByFieldId);
+
+router.route('/').post(authMiddleware.authenticateToken, availablereservationController.createAvailableReservation);
+router.route('/bulk').post(authMiddleware.authenticateToken, availablereservationController.createBulkAvailableReservations);
+
+
 
 module.exports = router;
