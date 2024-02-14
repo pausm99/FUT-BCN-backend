3684ad213b9e7a26b69b8ab4562fa4d3dcdf411c
pausm99 <pausm99@gmail.com>
2024-02-14 13:18:54 +0100

    ADD: logout endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/register', registerValidator, userController.register);
 router.post('/login', loginValidator, userController.login);
+router.post('/logout', userController.logout);
 
 router.route('by/Mail/:email').get(userController.getUserByEmail);
 router.route('/:id').get(userController.getUserById);
