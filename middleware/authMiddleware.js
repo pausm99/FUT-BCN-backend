478bd0baf2be4e0b3bf99e03c79bb30b87da1783
pausm99 <pausm99@gmail.com>
2024-02-20 16:16:42 +0100

    ADD: renewal of token when expire date less than 1 day

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 
 function authenticateToken(req, res, next) {
+
     const token = req.cookies.refreshToken;
 
     if (!token) {
@@ -13,9 +14,34 @@ function authenticateToken(req, res, next) {
             return res.status(403).json({ error: 'Invalid token, forbidden access' });
         }
 
+        const expire = user.exp;
+        const currentTimestamp = Math.floor(new Date().getTime() / 1000);
+        const differenceInSeconds = expire - currentTimestamp;
+
+        const payload = user.userSimple;
+
+
+        //less than 1 day from token to expire --> create new token
+        if (differenceInSeconds/60/60/24 < 1) {
+
+            const token = jwt.sign({ payload }, SECRET_KEY, {
+                expiresIn: '7d',
+            });
+
+            res.cookie('refreshToken', token, {
+                httpOnly: true, 
+                maxAge: 604800000,
+                path: '/',
+                secure: false,
+                //secure: true, CHANGE WHEN HTTPS
+            });
+        }
+
         req.user = user;
         next();
     });
+
+
 }
 
 module.exports = { authenticateToken };
